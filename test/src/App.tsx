5dc4a5d95b82e950ccf689e80528ce9540109185
MichaelHeinzman <seth@heinzman.net>
2025-05-05 22:05:37 -0400

    Updated test react repo

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -2,7 +2,7 @@ import Overlay from "./Overlay";
 import RenderCounter from "./RenderCounter";
 import { RLProvider } from "@four-leaf-studios/rl-socket-hook";
 import Scoreboard from "./Scoreboard";
-import WebsocketData from "@four-leaf-studios/rl-socket-hook";
+import { WebsocketData } from "@four-leaf-studios/rl-socket-hook";
 
 function App() {
   return (
